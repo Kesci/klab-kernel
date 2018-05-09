a07fb2c4bcb64b45a6eee9f4e5a84bbc53853ec1
Peng Gao <peng.gao.dut@gmail.com>
2018-05-09 19:29:04 +0800

    pip install pyecharts 0.2.7
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 files=$(git diff-tree --no-commit-id --name-only -r HEAD | grep Dockerfile)
+echo "modified files $files"
 for file in $files ; do
 	cd $(dirname $file)
 	name=$(basename $(dirname $file))
