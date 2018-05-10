293c3cfdb99d60475f58abf82774b99a5f359a28
Peng Gao <peng.gao.dut@gmail.com>
2018-05-10 15:03:00 +0800

    Install stable paddlepaddle 0.11.0
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+git diff-tree --no-commit-id --name-only -r HEAD
+git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH
 files=$(git diff-tree --no-commit-id --name-only -r HEAD | grep Dockerfile)
+file=$(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH)
+echo "modified files $files"
 for file in $files ; do
 	cd $(dirname $file)
 	name=$(basename $(dirname $file))
 	docker build -t klabteam/$name .
-	docker push klabteam/$name
 	cd -
 done 
--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,8 @@
 # default travis own is 2000:2000
 echo "testing py2 imports"
 python2 test_import_py2.py
+echo "testing paddlepaddle"
+python2 paddle_p2.py
 echo "testing py3 imports"
 python3 test_import_py3.py
 echo "testing torch imports(torch has conflict with tensorflow)"
