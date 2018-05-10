71e6b057827642be0d125d4c14bc82ee33c9c189
Peng Gao <peng.gao.dut@gmail.com>
2018-05-10 16:03:13 +0800

    Build klab only
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-git diff-tree --no-commit-id --name-only -r HEAD
-git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH
-files=$(git diff-tree --no-commit-id --name-only -r HEAD | grep Dockerfile)
-file=$(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH)
-echo "modified files $files"
-for file in $files ; do
-	cd $(dirname $file)
-	name=$(basename $(dirname $file))
-	docker build -t klabteam/$name .
-	cd -
-done 
+cd base/klab
+docker build -t klabteam/klab .
+cd -
