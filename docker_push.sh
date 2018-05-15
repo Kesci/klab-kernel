e9184f73734408f003085834b8615282f35a0cf7
Peng Gao <peng.gao.dut@gmail.com>
2018-05-15 19:31:22 +0800

    update ci
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- docker_push.sh
+++ docker_push.sh
@@ -7,5 +7,10 @@ if [ "$1" == "tags" ]; then
 	tag=$(git describe --abbrev=0 --tags)
 fi
 
+if [ "$1" == "gpu-master" ]; then
+	tag="latest-gpu"
+fi
+
+
 docker tag  klabteam/klab klabteam/klab:$tag
 docker push klabteam/klab:$tag
