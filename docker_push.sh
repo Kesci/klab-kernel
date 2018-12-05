146d952b0db2b1722fd64dba71ed21c220a1181d
Peng Gao <peng.gao.dut@gmail.com>
2018-12-05 16:38:39 +0800

    update docker ci
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- docker_push.sh
+++ docker_push.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-tag="latest"
+tag="$(git rev-parse --short HEAD)"
 
 if [ "$1" == "tags" ]; then
 	tag=$(git describe --abbrev=0 --tags)
 fi
+
+docker tag  klabteam/base klabteam/base:$tag
+docker push klabteam/base:$tag
 
 docker tag  klabteam/klab klabteam/klab:$tag
 docker push klabteam/klab:$tag
