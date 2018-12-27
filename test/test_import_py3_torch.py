8d8101545d03db2c50a3517af34738a85f8d3981
Peng Gao <peng.gao.dut@gmail.com>
2018-12-27 11:36:04 +0800

    update pandas and python3.6 [skip ci]
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- test/test_import_py3_torch.py
+++ test/test_import_py3_torch.py
@@ -7,3 +7,6 @@ linear_torch = tnn.Linear(5,3)
 data_torch = autograd.Variable(torch.randn(2, 5))
 print(linear_torch(data_torch))
 print("PyTorch ok")
+
+from allennlp.models import Model
+print("allennnlp ok")
