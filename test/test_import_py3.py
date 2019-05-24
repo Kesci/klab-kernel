e3fd9ca2f8ac7e5bc98c94c1f8094f44aba1b207
Peng Gao <peng.gao.dut@gmail.com>
2019-05-24 22:27:59 +0800

    fix
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- test/test_import_py3.py
+++ test/test_import_py3.py
@@ -183,13 +183,4 @@ import autosklearn
 print("autosklearn ok")
 
 import fasttext
-
-# Skipgram model
-model = fasttext.skipgram('data.txt', 'model')
-print model.words # list of words in dictionary
-
-# CBOW model
-model = fasttext.cbow('data.txt', 'model')
-print model.words # list of words in dictionary
-
 print("fasttext ok")
